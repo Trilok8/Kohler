ac8cb414d7b9ad8d1627acb5bf6b183c67e70ffd
Trilok8 <58468754+Trilok8@users.noreply.github.com>
2023-01-11 11:54:51 +0400

    Initial Commit

---
+++ BNIVideoBooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BNIVideoBooth
+//
+//  Created by Altaf Razzaque on 11/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
